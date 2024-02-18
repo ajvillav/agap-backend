2de9204f143cd9f26d51465ec3bc265d04436f73
Andres Jose Villa Vasquez <Andres.Villa@endava.com>
2024-02-17 20:25:31 -0500

    Initial structure for Agap backend API

---
+++ src/main/java/com/agapbackend/AgapBackendApplication.java
@@ -0,0 +1,13 @@
+package com.agapbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgapBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agapbackend/AgapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agapbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgapBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
